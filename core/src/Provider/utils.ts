9ca440deed46f6d90ad95d7345455f572838b935
SunLxy <1011771396@qq.com>
2023-11-22 18:54:02 +0800

    doc:更新说明

--- core/src/Provider/utils.ts
+++ core/src/Provider/utils.ts
@@ -14,7 +14,13 @@ export class ProviderInstance<T> extends Utils_Selector {
     this.store = initalValue || {}
   }
 
-  /**更新值*/
+  /**
+   * 更新值
+   * @param data 更新数据
+   * @param isUpdate 是否更新依赖数据的组件(默认：true)
+   * @param updateAll 是否更新所有组件(默认：false)
+   * 
+  */
   update = (data: Partial<T>, isUpdate = true, updateAll = false) => {
     if (Object.prototype.toString.call(data) === "[object Object]") {
       Object.entries(data).forEach(([key, value]) => {
